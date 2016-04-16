c40eabe365892b8a1e20ca39c95cd27e5473c7c2
Salina Wu <salinawu@uchicago.edu>
2016-04-16 18:38:58 +0200

    webscraping script

---
+++ nokogiri_tutorial/web_scraper.rb
@@ -0,0 +1,9 @@
+require 'HTTParty'
+require 'Nokogiri'
+require 'JSON'
+require 'Pry'
+require 'csv'
+
+page = HTTParty.get('https://incidentreports.uchicago.edu/incidentReportArchive.php' + '?startDate=04%2F11%2F2016&endDate=04%2F14%2F2016')
+parse_page = Nokogiri::HTML(page)
+Pry.start(binding)
