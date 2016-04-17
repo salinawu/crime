6960158e89eaa9a2c6772f04b1e98bf73da098f0
klu1622 <kathleenlu.26@gmail.com>
2016-04-17 18:14:08 +0200

    got number of pages

--- nokogiri_tutorial/web_scraper.rb
+++ nokogiri_tutorial/web_scraper.rb
@@ -3,7 +3,20 @@ require 'Nokogiri'
 require 'JSON'
 require 'Pry'
 require 'csv'
-#comment
-page = HTTParty.get('https://incidentreports.uchicago.edu/incidentReportArchive.php' + '?startDate=04%2F11%2F2016&endDate=04%2F14%2F2016')
+
+url = 'https://incidentreports.uchicago.edu/incidentReportArchive.php'
+start_day = 4
+start_month = 11
+start_year = 2016
+end_day = 4
+end_month = 14
+end_year = 2016
+offset = 0
+dates = "?startDate=#{start_day}%2F#{start_month}%2F#{start_year}&endDate=#{end_day}%2F#{end_month}%2F#{end_year}&offset=#{offset}"
+
+page = HTTParty.get(url + dates)
 parse_page = Nokogiri::HTML(page)
+data = parse_page.css(".ucpd")
+num_pages = parse_page.css(".page-count").text.split("/")[1].strip
+
 Pry.start(binding)
