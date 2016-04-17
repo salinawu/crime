81a36ba2892a331ac121083a9d6701e86914677e
Salina Wu <salinawu@uchicago.edu>
2016-04-17 17:15:56 +0200

    changed home page

--- nokogiri_tutorial/web_scraper.rb
+++ nokogiri_tutorial/web_scraper.rb
@@ -3,7 +3,7 @@ require 'Nokogiri'
 require 'JSON'
 require 'Pry'
 require 'csv'
-#comment
+
 page = HTTParty.get('https://incidentreports.uchicago.edu/incidentReportArchive.php' + '?startDate=04%2F11%2F2016&endDate=04%2F14%2F2016')
 parse_page = Nokogiri::HTML(page)
 Pry.start(binding)
