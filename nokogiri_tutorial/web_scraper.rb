2715b331e708c5636c3955f3a10ce82e25c7b48b
klu1622 <kathleenlu.26@gmail.com>
2016-04-19 18:16:29 +0200

    web scrapping works now

--- nokogiri_tutorial/web_scraper.rb
+++ nokogiri_tutorial/web_scraper.rb
@@ -1,8 +1,23 @@
-require 'HTTParty'
 require 'Nokogiri'
 require 'JSON'
 require 'Pry'
 require 'csv'
+require 'open-uri'
+
+#gets the type of incident, time of occurence, further explaination (comments), and disposition
+def extract(column)
+	array = []
+	i = 0
+	column.each { |a|
+		if i == 2 || i==6
+			i+=1
+			next
+		end
+			i+=1
+			array.push(a.text.delete("\n")) }
+	return array
+end
+
 
 url = 'https://incidentreports.uchicago.edu/incidentReportArchive.php'
 start_day = 4
@@ -14,9 +29,18 @@ end_year = 2016
 offset = 0
 dates = "?startDate=#{start_day}%2F#{start_month}%2F#{start_year}&endDate=#{end_day}%2F#{end_month}%2F#{end_year}&offset=#{offset}"
 
-page = HTTParty.get(url + dates)
-parse_page = Nokogiri::HTML(page)
-data = parse_page.css(".ucpd")
-num_pages = parse_page.css(".page-count").text.split("/")[1].strip
+parse_page = Nokogiri::HTML(open(url + dates))
+num_pages = Integer(parse_page.css(".page-count").text.split("/")[1].strip) -1
+table = parse_page.css(".ucpd tbody")
+
+cols = []
+for i in 0..num_pages
+	offset = 5*i
+	dates = "?startDate=#{start_day}%2F#{start_month}%2F#{start_year}&endDate=#{end_day}%2F#{end_month}%2F#{end_year}&offset=#{offset}"
+	parse_page = Nokogiri::HTML(open(url + dates))
+	num_pages = Integer(parse_page.css(".page-count").text.split("/")[1].strip) -1
+	table = parse_page.css(".ucpd tbody")
+	table.each { |a| cols.push(extract(a.css("td"))) }
+end
 
 Pry.start(binding)
