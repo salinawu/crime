029d1afe26ad094fc577ca67ed1c273ea99427ef
klu1622 <kathleenlu.26@gmail.com>
2016-06-18 18:49:25 -0700

    fixed the address search js issues / should be working / i am dumb

--- crime/app/assets/javascripts/common.js
+++ crime/app/assets/javascripts/common.js
@@ -47,10 +47,8 @@ function addMarker(map, marker_geocoder, crime) {
 
       markersArray.push(marker);
 
-      // google.maps.event.addListener(marker,"click",function(){});
-
     } else if (status === google.maps.GeocoderStatus.OVER_QUERY_LIMIT) {
-      setTimeout(function() { addMarker(map, marker_geocoder, crime) }, 200);
+      setTimeout(function() { addMarker(map, marker_geocoder, crime) }, 100);
     } else {
       alert("Geocode was not successful for the following reason: " + status);
     }
@@ -82,10 +80,10 @@ class GSC {
     return deg * (Math.PI / 180);
   }
 
-  isWithInPointFourMiles(other) {
+  isWithInPointFourMiles(other, dist) {
     return  ( 3959 * Math.acos( Math.cos( this.degreesToRadians(this.lat) ) * Math.cos( this.degreesToRadians(other.lat) ) * 
       Math.cos( this.degreesToRadians(other.long) - this.degreesToRadians(this.long) ) + 
-      Math.sin( this.degreesToRadians(this.lat) ) * Math.sin( this.degreesToRadians(other.lat) ) ) ) <= .4;
+      Math.sin( this.degreesToRadians(this.lat) ) * Math.sin( this.degreesToRadians(other.lat) ) ) ) <= dist;
   }
 }
 
@@ -94,8 +92,7 @@ function getLongLat(crime, targetGSC) {
   marker_geocoder.geocode({'address': crime["Location"] + " Chicago 60637"}, function(results, status) {
     if (status === google.maps.GeocoderStatus.OK) {
       var curGSC = new GSC(results[0].geometry.location.lng(), results[0].geometry.location.lat());
-      if (targetGSC.isWithInPointFourMiles(curGSC)) {
-
+      if (targetGSC.isWithInPointFourMiles(curGSC, .4)) {
         var marker = new google.maps.Marker({
           map: map,
           position: results[0].geometry.location
@@ -111,24 +108,32 @@ function getLongLat(crime, targetGSC) {
         markersArray.push(marker);
       }
     } else if (status === google.maps.GeocoderStatus.OVER_QUERY_LIMIT) {
-      setTimeout(function() { getLongLat(crime["Location"]) }, 200);
+      setTimeout(function() { getLongLat(crime, targetGSC) }, 100);
     }
   });
 }
 
+//bc UCPD's data is forever messed up. must check that crimes are not void or empty
+function isValidEntry(crime) {
+  return !crime["Location"] ||
+        (crime["Location"] && crime["Location"].indexOf(":") === -1 && 
+          crime["Location"].toLowerCase().indexOf("void") === -1);
+}
 
 function markWithinXDist(addresses, targetAddress) {
 
   if (addresses.length == 0) {
     $("#notes").html("<h5>A crime-free time! </br> (At least around here.) </h5>");
   } else {
+
     marker_geocoder.geocode({'address': targetAddress + " Chicago 60637"}, function(results, status) {
       if (status === google.maps.GeocoderStatus.OK) {
         var targetGSC = new GSC(results[0].geometry.location.lng(), results[0].geometry.location.lat());
-        for (var i = 0; i < addresses.length; i++) {
-          if (addresses[i]["Location"] && addresses[i]["Location"].indexOf(":") === -1 && 
-            addresses[i]["Location"].toLowerCase().indexOf("void") === -1) {
-             getLongLat(addresses[i], targetGSC);
+        if (typeof targetGSC !== undefined) {
+          for (var i = 0; i < addresses.length; i++) {
+            if (isValidEntry(addresses[i])) {
+               getLongLat(addresses[i], targetGSC);
+            }
           }
         }
       }
@@ -144,18 +149,15 @@ $(document).ready(function() {
 
       $.post($("#form").attr("action"), $("#form").serialize(), function(data) {
 
-        if (data.hasOwnProperty("address")) {
-          clearOverlays();
-          addMarker(map, marker_geocoder, data["address"] + " Chicago 60637");
-        } else if (data.hasOwnProperty("notes")) {
+        if (data.hasOwnProperty("notes")) {
           $("#notes").html(data["notes"]);
-        } else if (data.hasOwnProperty("searchaddr")) {
+        } else if (data.hasOwnProperty("addresses")) {
           clearOverlays();
-          markWithinXDist(data["addresses"], data["searchaddr"]);
+          markWithinXDist(data["addresses"], document.getElementById("address").value);
         } else {
           clearOverlays();
           $.each(data, function(index, addressses) {
-              addMarker(map, marker_geocoder, data[index]);
+            addMarker(map, marker_geocoder, data[index]);
           });
         } 
 
